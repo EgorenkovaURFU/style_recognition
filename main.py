51fc7c65af7f26d99062272652897dbd8e3885e5
Tanya <tatyana-baeva@list.ru>
2024-02-19 13:13:39 +0700

    add flake8 and correct code

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ async def read_items():
 
 
 def load_model(path: str):
-  model = torchvision.models.resnet50(num_classes=6)
-  model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
-  return model
+    model = torchvision.models.resnet50(num_classes=6)
+    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    return model
 
 
 def save_image(url):
@@ -38,50 +38,49 @@ def save_image(url):
 
 
 def picture_prepare(picture_path):
-  img = Image.open(picture_path).convert('RGB')
+    img = Image.open(picture_path).convert('RGB')
 
-  transform = transforms.Compose([
-              transforms.Resize(size=224, interpolation=Image.BILINEAR),
-              transforms.CenterCrop(size=(224, 224)),
-              transforms.ToTensor(),
-              transforms.Normalize(
+    transform = transforms.Compose([
+                transforms.Resize(size=224, interpolation=Image.BILINEAR),
+                transforms.CenterCrop(size=(224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
                   mean=[0.49333772, 0.51176786, 0.51791704],
                   std=[0.26378724, 0.26562205, 0.3115852]
                   )
               ])
 
-  img = transform(img)
-  img = torch.unsqueeze(img, 0)
-  return img
+    img = transform(img)
+    img = torch.unsqueeze(img, 0)
+    return img
 
 
 def predict(model, image, labels):
-  model.eval()
-  out = model(image)
-  with open(labels) as f:
-      labels = [line.strip() for line in f.readlines()]
+    model.eval()
+    out = model(image)
+    with open(labels) as f:
+        labels = [line.strip() for line in f.readlines()]
 
-  _, index = torch.max(out, 1)
-  percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-  return(labels[index[0]], percentage[index[0]].item())
+    _, index = torch.max(out, 1)
+    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    return (labels[index[0]], percentage[index[0]].item())
 
 
 @app.post("/prediction")
 async def get_net_image_prediction(request: Request, url=Form()):
-  if url == "":
-    return {"message": "No image link provided"}
-    
-  save_image(url)
+    if url == "":
+        return {"message": "No image link provided"}
 
-  image = picture_prepare('static/image_name.jpg')
-  model = load_model('wc6_224_balanced.pth')
-  labels = 'lab.txt'
-  prediction, score = predict(model, image, labels)
-  context = {'request': request, 'prediction': prediction, 'score': score}
+    save_image(url)
 
-  return templates.TemplateResponse("item.html", context)
+    image = picture_prepare('static/image_name.jpg')
+    model = load_model('wc6_224_balanced.pth')
+    labels = 'lab.txt'
+    prediction, score = predict(model, image, labels)
+    context = {'request': request, 'prediction': prediction, 'score': score}
+
+    return templates.TemplateResponse("item.html", context)
 
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="127.0.0.1", port=5000)
-
+    uvicorn.run("main:app", host="127.0.0.1", port=5000)
