ab156b88d5550d3c2b4e914d5c5fa7d7e91ad4ad
VasiliVorobev <vvorobiev@bk.ru>
2024-05-11 19:43:25 +0300

    project updated

--- main.py
+++ main.py
@@ -1,46 +1,20 @@
+import streamlit as st
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
-
 from PIL import Image
 
-from fastapi import FastAPI, Form, Request
-import requests
-import uvicorn
-
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from src import load_model, predict
-
-MODEL_PATH = 'models/wc6_224_balanced.pth'
-
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
+import os
 
 
-@app.get("/", response_class=HTMLResponse)
-async def read_items():
-    return FileResponse('templates/index.html')
-
-
-
-def save_image(url):
-    print(url)
-    img_data = requests.get(url).content
-    print(img_data)
-    with open('static/image_name.jpg', 'wb') as handler:
-        handler.write(img_data)
-
-
-def picture_prepare(picture_path):
-    img = Image.open(picture_path).convert('RGB')
+def picture_prepare(img_source):
+    #img = Image.open(img_source).convert('RGB')
+    img = img_source.convert('RGB')
 
     transform = transforms.Compose([
-                transforms.Resize(size=224, interpolation=Image.BILINEAR),
+                #transforms.Resize(size=224, interpolation=InterpolationMode.BILINEAR),
+                transforms.Resize(size=224),
                 transforms.CenterCrop(size=(224, 224)),
                 transforms.ToTensor(),
                 transforms.Normalize(
@@ -48,28 +22,94 @@ def picture_prepare(picture_path):
                   std=[0.26378724, 0.26562205, 0.3115852]
                   )
               ])
+    img_tensor = transform(img)
+    img_tensor = torch.unsqueeze(img_tensor, 0)
+    #return {'img': img, 'img_tensor': img_tensor}
+    return img, img_tensor
 
-    img = transform(img)
-    img = torch.unsqueeze(img, 0)
-    return img
+def save_uploadedfile(uploadedfile, path):
+    with open(os.path.join( path, uploadedfile.name), 'wb') as f:
+        f.write(uploadedfile.getbuffer())
+        return st.success('Файл сохранен'.format(uploadedfile.name))
 
 
 
-@app.post("/prediction")
-async def get_net_image_prediction(request: Request, url=Form()):
-    if url == "":
-        return {"message": "No image link provided"}
+def load_image(uploaded_file):
+    image = Image.open(uploaded_file)
+    return image
 
-    save_image(url)
+def load_file():
+    uploaded_file = st.file_uploader(label='Выберите файл')
 
-    image = picture_prepare('static/image_name.jpg')
-    model = load_model(MODEL_PATH)
+    if uploaded_file is not None:
+        save_uploadedfile(uploaded_file, path)
+        st.session_state.stage = 0
+        return uploaded_file
+    else:
+        return None
+
+def load_model(path: str):
+    model = torchvision.models.resnet50(num_classes=6)
+    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    return model
+
+def predict(model, image, labels):
+    model.eval()
+    out = model(image)
+    with open(labels) as f:
+        labels = [line.strip() for line in f.readlines()]
+
+    _, index = torch.max(out, 1)
+    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    return (labels[index[0]], percentage[index[0]].item())
+
+
+#Основное тело программы
+
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+path = 'Pictures/'  # Папка с изображениями
+
+if st.button('Добавить изображение в коллекцию из файла'):
+    print(1)
+    st.session_state.stage = 1
+
+if st.session_state.stage == 1:
+    load_file()
+    #file = load_file()
+
+
+if st.button('Проанализировать изображения'):
+    image_list = []
+    image_pr_list = []
+    image_tensor_list = []
+    prediction_list = []
+    score_list = []
+
+    model = load_model('models\wc6_224_balanced.pth')
     labels = 'lab.txt'
-    prediction, score = predict(model, image, labels)
-    context = {'request': request, 'prediction': prediction, 'score': score}
-
-    return templates.TemplateResponse("item.html", context)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000)
+
+    for file in os.listdir(path):
+        print(path, '   ', file)
+        if file.endswith(".jpg"):
+            print(type(file.endswith))
+            image_loaded = load_image(path + file)
+            st.image(image_loaded)
+            image_list.append(image_loaded)
+
+            image_pr, image_tensor = picture_prepare(image_loaded)
+            #st.image(image_pr)
+            image_pr_list.append(image_pr)
+            image_tensor_list.append(image_tensor)
+
+            prediction, score = predict(model, image_tensor, labels)
+            st.success(prediction)
+            st.success(score)
+            prediction_list.append(prediction)
+            score_list.append(score)
+
+    print(prediction_list)
+    print(score_list)
